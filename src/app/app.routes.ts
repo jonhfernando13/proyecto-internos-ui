55880099cc4d7d25cf42eabe107f402f337302e6
jonh <jonhfernando13@gmail.com>
2026-01-15 17:20:44 -0500

    actualizaciones

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,7 +3,7 @@ import { InternosComponent } from './pages/internos/internos.component';
 import { InternoFormComponent } from './pages/interno-form/interno-form.component';
 
 export const routes: Routes = [
-  { path: '', redirectTo: 'internos/nuevo', pathMatch: 'full' },
+  { path: '', redirectTo: 'internos', pathMatch: 'full' },
   { path: 'internos', component: InternosComponent },
   { path: 'internos/nuevo', component: InternoFormComponent },
   { path: 'internos/editar/:id', component: InternoFormComponent },
